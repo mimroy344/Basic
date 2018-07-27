28cac5239ab5259cd7eaa20e0d071f79d35d07b3
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-07-27 16:37:23 +0600

    Create Variable&Datatype.js

---
+++ Variable&Datatype.js
@@ -0,0 +1,17 @@
+/*.....................................
+ simple Variable & data type example
+.......................................*/
+
+var name = 'Mim Roy';
+var roll = 290229;
+var student = true;
+var status = null;
+var address;
+
+console.log(name , typeof name);
+console.log(roll,typeof roll);
+console.log(student,typeof student);
+console.log(status,typeof status);
+console.log(address,typeof address);
+
+
