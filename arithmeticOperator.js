f1777a0216bffd0ab1cc88d76a5aa50ea30a0db8
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-01 16:18:48 +0600

    Update and rename Operator.js to arithmeticOperator.js

--- arithmeticOperator.js
+++ arithmeticOperator.js
@@ -1,34 +1,25 @@
-/*********************
-Operator  +,-,*,/,%
-***********************/
+/**********************************
+  Operator  +, -, *, /, %, ++, --
+***********************************/
 //Cost of eggs -calculator
 var priceOfegg = 5;
 var numberOfEggs=10;
 var total = priceOfegg*numberOfEggs;
-
 console.log('Total cost of eggs',total,'tk');
 
 //circle of area
 var radius = 7;
 const pi = 3.14159;
 var areaOfCircle = pi * (radius * radius);
-
 console.log("Area of the Circle is",areaOfCircle,'sq inch');
 
 // usd to bdt calculator
 var usdToBdt = 83.5;
 var bdt = 5000;
 var usd = bdt/usdToBdt;
-
 console.log("$",usd);
 
 // Modulas
 var n1= 5;
 var r1=5%2;
-
 console.log(r1);
-
-// Even
-
-
-// Odd
