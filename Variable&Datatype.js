cddd6e4b8cb3778d0f881c3df0237f3d3961dabe
Mim Roy <41783254+mimroy344@users.noreply.github.com>
2018-08-01 18:58:25 +0600

    Update Variable&Datatype.js

--- Variable&Datatype.js
+++ Variable&Datatype.js
@@ -1,18 +1,19 @@
 /********************
  Variable Declaration
  *********************/
-var a = 2;
-var b = 2;
-var c = 2;
-var d = 4;
-
-var a = 2,b = 2,c = 2,d = 4;
-
-var a = b = c = 2;
-var d = 4;
-
-console.log(a,b,c);
-
+/*.....................................
+        data type example
+.......................................*/
+var a = 14;
+var b =14;
+var c = 14;
+var d = 15
+//data assign of Multiple Variable in one line
+var a = 14,b = 14,c = 14,d = 15;
+//simple 
+var a = b = c =14;
+var d = 15
+console.log(a,b,c,d);
 /****************
     Data type
 ******************
@@ -26,15 +27,19 @@ javascript has 7 data type 6 data type that are primitives:
 6.Symbol    and 
 7.Object
 */
-
-var exponential=1e2;
-var n= -12;
-var earthIsRound = true;
+/*.....................................
+ simple data type example
+.......................................*/
 var word = "earth";
-const pi = 3.14150;
-
-console.log(typeof pi);
-
+var exponential=1e2;
+var earthIsRound = true;
+var status = null;
+var address;
+console.log(typeof word);
+console.log(typeof exponential);
+console.log(typeof earthIsRound);
+console.log(typeof status);
+console.log(address,typeof address);
 /*************problem****************/
 var a =1;
 var c = a+++1;
@@ -57,34 +62,4 @@ var a = "1"; // string
 a = a + 0; //  "10" the + operator concatenate the strings
 a = a - 1; // 9 the - operators convert string to a number
 a = a + 1; // 10
-
 var a = -1; var b = 0.25; alert( a == ~b^0 );
-
-/*.....................................
- simple Variable & data type example
-.......................................*/
-
-var name = 'Mim Roy';
-var roll = 290229;
-var student = true;
-var status = null;
-var address;
-
-console.log(name , typeof name);
-console.log(roll,typeof roll);
-console.log(student,typeof student);
-console.log(status,typeof status);
-console.log(address,typeof address);
-/*.....................................
-        data type example
-.......................................*/
-var a = 14;
-var b =14;
-var c = 14;
-var d = 15
-//data assign of Multiple Variable in one line
-var a = 14,b = 14,c = 14,d = 15;
-//simple 
-var a = b = c =14;
-var d = 15
-console.log(a,b,c,d);
